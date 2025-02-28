efddc04decbdb82c692618f03954acb494f3a583
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2025-02-28 15:18:00 +0100

    ff

--- backend/src/auth/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AuthService {
-
-    async getMessages(id: any): Promise<string> {
-        return 'Hello World from auth! Number: ' + id;
-    }
-}
